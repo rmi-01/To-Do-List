b32f56f0897d1b9f3ebaddb2b8cbf55e8fac6cf2
ebuildSlayer <mohammadibrahim.ibrahim9@gmail.com>
2020-04-10 15:38:42 +0500

    Updates

---
+++ precache-manifest.0b17e35b1a3310c4efebac3a55daa691.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e72d33cb9057bce92651f09ef4a0d510",
+    "url": "/To-Do-List/index.html"
+  },
+  {
+    "revision": "560b439153b669a17b9d",
+    "url": "/To-Do-List/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "7ec04be17727a2e21d30",
+    "url": "/To-Do-List/static/css/main.e32c0ed1.chunk.css"
+  },
+  {
+    "revision": "560b439153b669a17b9d",
+    "url": "/To-Do-List/static/js/2.e2880853.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-List/static/js/2.e2880853.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ec04be17727a2e21d30",
+    "url": "/To-Do-List/static/js/main.74dc1522.chunk.js"
+  },
+  {
+    "revision": "394dc37eaa602265055a",
+    "url": "/To-Do-List/static/js/runtime-main.e1c48ba3.js"
+  }
+]);
